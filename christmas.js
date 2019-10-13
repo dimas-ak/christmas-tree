bede39b32088cf19fc0cc0b3641ea2a260a1452d
Dimas Awang Kusuma <arjunane.kajen@gmail.com>
2019-10-13 19:29:58 +0700

    Update christmas.js

--- christmas.js
+++ christmas.js
@@ -1,54 +1,64 @@
-var cont = document.querySelector(".container");
-
-cont.innerHTML = tree(3);
-
-setTimeout(() => {
-
-    var star = document.querySelectorAll(".star");
-
-    for(var s = 0; s < star.length; s++)
+(function (a) {
+    var cont = null;
+    function christmas(elem, leaf)
     {
-        var ini = star[s];
-        addClass(ini, s);
-    }
-    
-}, 500);
-
-function tree(daun)
-{
-    var wood = 5 + daun,
-        elem = '<div class="wrap"><div class="first">*</div></div>';
-
-    for(var i = 0; i < daun; i++)
-    {
-
-        for(var t = 0; t < 10; t++)
+        return function (elem, leaf)
         {
-
-            elem += '<div class="wrap">';
-
-            for(var d = 0; d <= t + i; d++)
-            {
-                elem += '<div class="star">*</div>';
-            }
-
-            elem += "</div>";
+            cont = document.querySelector(elem);
+            return cont.innerHTML = tree(leaf);
         }
     }
 
-    for(var w = 0; w < wood; w++)
+    setTimeout(() => {
+    
+        var star = document.querySelectorAll(".star");
+    
+        for(var s = 0; s < star.length; s++)
+        {
+            var ini = star[s];
+            addClass(ini, s);
+        }
+        
+    }, 500);
+    
+    function tree(daun)
     {
-        elem += '<div class="wrap">' + 
-                    '<div class="star">***</div>' + 
-                '</div>';
+        var wood = 5 + daun,
+            elem = '<div class="wrap"><div class="first">*</div></div>';
+
+        for(var i = 0; i < daun; i++)
+        {
+
+            for(var t = 0; t < 10; t++)
+            {
+
+                elem += '<div class="wrap">';
+
+                for(var d = 0; d <= t + i; d++)
+                {
+                    elem += '<div class="star">*</div>';
+                }
+
+                elem += "</div>";
+            }
+        }
+
+        for(var w = 0; w < wood; w++)
+        {
+            elem += '<div class="wrap">' + 
+                        '<div class="star">***</div>' + 
+                    '</div>';
+        }
+
+        return elem;
     }
 
-    return elem;
-}
+    function addClass(elem, index)
+    {
+        setTimeout(() => {
+            elem.className += ' aktif';
+        }, 50 * index);
+    }
+    a.christmas = christmas()
 
-function addClass(elem, index)
-{
-    setTimeout(() => {
-        elem.className += ' aktif';
-    }, 50 * index);
-}
+})(window);
